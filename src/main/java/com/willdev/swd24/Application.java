4db3c03a16955bf974c181d5cf09d05e47070a6b
WillOli <william.ika@gmail.com>
2024-03-30 11:30:34 -0300

    first commit

---
+++ src/main/java/com/willdev/swd24/Application.java
@@ -0,0 +1,13 @@
+package com.willdev.swd24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/willdev/swd24/SantanderWeekDev2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willdev.swd24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderWeekDev2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
