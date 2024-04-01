2db1606e9c4b5e91a5acff20bb7afa26ad8952da
WillOli <william.ika@gmail.com>
2024-04-01 00:10:33 -0300

    test: concluido

--- src/test/java/application/ListChampionsUserCaseIntegrationTest.java
+++ src/test/java/application/ListChampionsUserCaseIntegrationTest.java
@@ -1,15 +1,17 @@
 package application;
 
+import com.willdev.swd24.Application;
 import com.willdev.swd24.application.ListChampionsUseCase;
 import com.willdev.swd24.domain.model.Champions;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 import java.util.List;
 
-@SpringBootTest
+@SpringBootTest(classes = Application.class)
 public class ListChampionsUserCaseIntegrationTest {
 
     @Autowired
